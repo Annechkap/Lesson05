b37e485dc9251fb91caf2335a96fe020176f8018
Anna <annechkap@list.ru>
2022-10-20 15:51:42 +0300

    Добавлена задача 001

---
+++ 001/Program.cs
@@ -0,0 +1,36 @@
+﻿// 1. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000); ;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int[] myArray = CreateArray(5);
+PrintArray(myArray);
+
+int count = 0;
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"{count} из них чётные");
+
